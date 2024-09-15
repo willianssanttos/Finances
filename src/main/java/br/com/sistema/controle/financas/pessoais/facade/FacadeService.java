f9f5b65500f2e0664fe4ed5155586fc1e1ecca2b
wmatos <willianssantos227@gmail.com>
2024-09-15 00:07:28 -0300

    Realizando atualização de erro apos merge

--- src/main/java/br/com/sistema/controle/financas/pessoais/facade/FacadeService.java
+++ src/main/java/br/com/sistema/controle/financas/pessoais/facade/FacadeService.java
@@ -39,7 +39,7 @@ public class FacadeService {
             usuarioService.criarUsuario(usuario);
         } catch (Exception e) {
             e.printStackTrace();
-            throw new ServiceException(Constantes.ErroCadastroConta, e);
+            throw new ServiceException(Constantes.ErroCadastroUsuario, e);
         }
     }
 
--- src/main/java/br/com/sistema/controle/financas/pessoais/utils/Constantes.java
+++ src/main/java/br/com/sistema/controle/financas/pessoais/utils/Constantes.java
@@ -5,24 +5,16 @@ public class Constantes {
     public static final String bemVindo = "BEM VINDO";
     public static final String OpcaoInvalida = "Opção invalida, utilize somente os números mostrados no Menu!";
     public static final String cadastroNome = "Nome invalido! O nome deve conter somente letras, com no minimo 2 e no máximo 100 caracteres!";
-
     public static final String cadastroEmail = "Insira um E-mail válido!";
-
     public static final String cadastroCelular = "Insira o número de celular ou número fixo com DDD(dois digitos, ex: 11912341234)";
-
     public static final String cadastroSenha = "Senha inválida. A senha deve ter no mínimo 8 caracteres, contendo letras, números e pelo menos um caractere especial (@, #, %, &, $).";
-
     public static final String confirmacaoSenha = "Senha incorreta! Verifique a senha digitada.";
-
     public static final String CadastroRealizadoUsuario = "Cadastrado realizado com sucesso! ";
-
     public static final String MensagemLoginUsuario = "Utilize o email e a senha definida no cadastro para logar na conta";
 
     //mensagens de erros
     public static final String ErroCadastroUsuario = "Erro! Cadastrado não realizado!";
-
     public static final String EmailJaCadastrado = "Esse e-mail, já foi cadastrado!";
-
     public static final String ErroVerificarEmail = "O e-mail, não foi possivel ser verificado!";
     public static final String ErroCadastroConta = "Erro! Adicionar conta!";
     public static final String ContaEditada = "Conta editada com sucesso!";
@@ -30,49 +22,25 @@ public class Constantes {
     public static final String ErroEditar = "Erro ao editar conta";
     public static final String ErroExcluir = "Erro ao excluir conta";
 
-    public static final String erroSN = "Opção invalida! Digite 's', para sair ou 'n' para";
-
-
-
-
 ///Login ////
 
     public static final String loginConta = "Login realizado com sucesso!";
-
     public static final String erroLoginConta = "Usuário ou senha incorretos!";
-
-    public static final String usuarioNaoEncontrado = "Usuario não encontrado!";
     public static final String tipoTransacao = "Tipo transação invalido! Valores permitidos. Use 1 para receitas ou 2 para despesas!";
     public static final String cadastroTransacao = "Transação registrada com sucesso!";
-
     public static final String ErrocadastroTransacao = "Erro ao realizar registro transação!";
-
     public static final String ErrorRecuperarIdUsuario = "Erro ao recuperar o id do usuario!";
-
     public static final String ErrorRecuperarContas = "Erro ao recuperar contas do usuario!";
     public static final String ErrorRecuperarExtrato = "Erro ao recuperar extrato de transações!";
 
     ////// cadastro da conta //////
 
     public static final String contaNaoEncontrada = "Nenhuma conta encontrada para o usuário!";
-
     public static final String criarConta = "Cadastre uma conta, e começe acompanhar sua vida financeira!";
-
     public static final String cadastroNomeConta = "Insira o nome da conta";
     public static final String cadastroTipoConta = "Tipo contas invalido! Valores permitidos: CORRENTE\", \"POUPANÇA\", \"INVESTIMENTO, \"BENEFICIOS, \"OUTROS";
     public static final String cadastroSaldo = "O valor invalido!";
-
     public static final String cadastroConta = "Conta adicionada com sucesso!";
-    public static final String cadastroProdutoCodigo = "Insira o codigo do produto";
 
-    public static final String cadastroProdutoDescricao = "Insira a descrição do produto";
-
-
-
-    public static final String cadastroProdutoEstoque = "Insira o valor de estoque do produto";
-
-    public static  void aberturaSistema(){
-        System.out.println("MoneyMind");
-    }
 
 }
